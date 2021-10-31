b86653385e9a7df84f3885efab29c5efd3f2b11e
jan3zk <janez.krizzaj@gmail.com>
2021-10-31 16:48:53 +0100

    Modified indentenation in *.py files.d

--- gphotos_backup.py
+++ gphotos_backup.py
@@ -25,162 +25,162 @@ pd.set_option('display.width', 150)
 pd.set_option('expand_frame_repr', True)
 
 def download_file(url:str, destination_folder:str, file_name:str):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(os.path.join(destination_folder, file_name), 'wb') as f:
-            f.write(response.content)
-            f.close()
+  response = requests.get(url)
+  if response.status_code == 200:
+    with open(os.path.join(destination_folder, file_name), 'wb') as f:
+      f.write(response.content)
+      f.close()
 
 def gphotos_backup():
-    if os.path.isfile('client_secret*.json'):
-        credentialsFile = glob('client_secret*.json')[0]
+  if os.path.isfile('client_secret*.json'):
+    credentialsFile = glob('client_secret*.json')[0]
+  else:
+    credentialsFile = 'credentials.json'
+  pickleFile = 'token.pickle'
+
+  SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
+  creds = None
+  if os.path.exists(pickleFile):
+    with open(pickleFile, 'rb') as token:
+      creds = pickle.load(token)
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
     else:
-        credentialsFile = 'credentials.json'
-    pickleFile = 'token.pickle'
+      flow = InstalledAppFlow.from_client_secrets_file(
+        credentialsFile, SCOPES)
+      creds = flow.run_local_server()
+    with open(pickleFile, 'wb') as token:
+      pickle.dump(creds, token)
+  service = build('photoslibrary', 'v1', credentials=creds, cache_discovery=False, static_discovery=False)
 
-    SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-    creds = None
-    if os.path.exists(pickleFile):
-        with open(pickleFile, 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credentialsFile, SCOPES)
-            creds = flow.run_local_server()
-        with open(pickleFile, 'wb') as token:
-            pickle.dump(creds, token)
-    service = build('photoslibrary', 'v1', credentials=creds, cache_discovery=False, static_discovery=False)
+  # Get all album names
+  nextpagetoken = 'Dummy'
+  myAlbums_list = []
+  while nextpagetoken != '':
+    nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
+    myAlbums = service.albums().list(pageSize=10,pageToken=nextpagetoken).execute()
+    myAlbums_list.append(pd.DataFrame(myAlbums.get('albums')))
+    nextpagetoken = myAlbums.get('nextPageToken', '')
+  dfAlbums = pd.concat(myAlbums_list).reset_index(drop=True)
+  if args.albums[0] == 'all':
+    album_titles = dfAlbums['title']
+  else:
+    album_titles = args.albums
 
-    # Get all album names
+  # Backup albums one by one
+  for album_idx, album_title in enumerate(album_titles):
+    print('\nBacking-up album %s (%d/%d) ...'%(album_title, album_idx+1, len(album_titles)))
+    if not dfAlbums[dfAlbums['title'] == album_title]['id'].empty:
+      album_id = dfAlbums[dfAlbums['title'] == album_title]['id'].to_string(index=False).strip()
+    else:
+      print('Wrong album name.')
+      return
+    album_dir = os.path.join(args.dir,album_title)
+    Path(album_dir).mkdir(parents=True, exist_ok=True)
     nextpagetoken = 'Dummy'
-    myAlbums_list = []
+    img_idx = 1
+    file_names = []
+    f_cap = open(os.path.join(album_dir,'captions.csv'),'w')
     while nextpagetoken != '':
-        nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
-        myAlbums = service.albums().list(pageSize=10,pageToken=nextpagetoken).execute()
-        myAlbums_list.append(pd.DataFrame(myAlbums.get('albums')))
-        nextpagetoken = myAlbums.get('nextPageToken', '')
-    dfAlbums = pd.concat(myAlbums_list).reset_index(drop=True)
-    if args.albums[0] == 'all':
-        album_titles = dfAlbums['title']
-    else:
-        album_titles = args.albums
-
-    # Backup albums one by one
-    for album_idx, album_title in enumerate(album_titles):
-        print('\nBacking-up album %s (%d/%d) ...'%(album_title, album_idx+1, len(album_titles)))
-        if not dfAlbums[dfAlbums['title'] == album_title]['id'].empty:
-            album_id = dfAlbums[dfAlbums['title'] == album_title]['id'].to_string(index=False).strip()
+      nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
+      results = service.mediaItems().search(body={'albumId': album_id, 'pageToken': nextpagetoken}).execute()
+      media_files = results.get('mediaItems', [])  
+      nextpagetoken = results.get('nextPageToken', '')
+      for media_file in media_files:
+        file_name = media_file['filename']
+        file_names.append(file_name)
+        img_path = os.path.join(album_dir, file_name)
+        _, img_ext = os.path.splitext(img_path)
+        if img_ext in ['.avi','.AVI','.mp4','.MP4','.mov','.MOV','.vmw','.VMW']:
+          download_url = media_file['baseUrl'] + '=dv'
         else:
-            print('Wrong album name.')
-            return
-        album_dir = os.path.join(args.dir,album_title)
-        Path(album_dir).mkdir(parents=True, exist_ok=True)
-        nextpagetoken = 'Dummy'
-        img_idx = 1
-        file_names = []
-        f_cap = open(os.path.join(album_dir,'captions.csv'),'w')
-        while nextpagetoken != '':
-            nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
-            results = service.mediaItems().search(body={'albumId': album_id, 'pageToken': nextpagetoken}).execute()
-            media_files = results.get('mediaItems', [])  
-            nextpagetoken = results.get('nextPageToken', '')
-            for media_file in media_files:
-                file_name = media_file['filename']
-                file_names.append(file_name)
-                img_path = os.path.join(album_dir, file_name)
-                _, img_ext = os.path.splitext(img_path)
-                if img_ext in ['.avi','.AVI','.mp4','.MP4','.mov','.MOV','.vmw','.VMW']:
-                    download_url = media_file['baseUrl'] + '=dv'
-                else:
-                    download_url = media_file['baseUrl'] + '=d'
+          download_url = media_file['baseUrl'] + '=d'
 
-                # Download image if it doesn't already exist locally
-                if not os.path.isfile(img_path):
-                    dl_count = 0
-                    while dl_count < 3:
-                        try:
-                            print('Downloading file %s (%d).'%(file_name, img_idx), end='')
-                            download_file(download_url, album_dir, file_name)
-                        except:
-                            dl_count = dl_count + 1
-                            logging.exception('Connection error ...')
-                        else:
-                            break
-                else:
-                    print('Skip downloading file %s (%d).'%(file_name, img_idx), end='')
+        # Download image if it doesn't already exist locally
+        if not os.path.isfile(img_path):
+          dl_count = 0
+          while dl_count < 3:
+            try:
+              print('Downloading file %s (%d).'%(file_name, img_idx), end='')
+              download_file(download_url, album_dir, file_name)
+            except:
+              dl_count = dl_count + 1
+              logging.exception('Connection error ...')
+            else:
+              break
+        else:
+          print('Skip downloading file %s (%d).'%(file_name, img_idx), end='')
 
-                # Embed description into image metadata
-                if 'description' in media_file:
-                    user_comment = media_file['description']
-                    print(' Description: %s'%user_comment)
-                    if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
-                        exif_dict = piexif.load(img_path)
-                        exif_dict["0th"][piexif.ImageIFD.ImageDescription] = bytes(user_comment, "utf-8")
-                        exif_dict["Exif"][piexif.ExifIFD.UserComment] = piexif.helper.UserComment.dump(user_comment,encoding='unicode')
-                        exif_bytes = piexif.dump(exif_dict) 
-                        piexif.insert(exif_bytes, img_path)
-                    elif img_ext in ['.png','.PNG']:
-                        targetImage = PngImageFile(img_path)
-                        metadata = PngInfo()
-                        metadata.add_text("User Comment", user_comment)
-                        targetImage.save(img_path, pnginfo=metadata)
+        # Embed description into image metadata
+        if 'description' in media_file:
+          user_comment = media_file['description']
+          print(' Description: %s'%user_comment)
+          if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
+            exif_dict = piexif.load(img_path)
+            exif_dict["0th"][piexif.ImageIFD.ImageDescription] = bytes(user_comment, "utf-8")
+            exif_dict["Exif"][piexif.ExifIFD.UserComment] = piexif.helper.UserComment.dump(user_comment,encoding='unicode')
+            exif_bytes = piexif.dump(exif_dict) 
+            piexif.insert(exif_bytes, img_path)
+          elif img_ext in ['.png','.PNG']:
+            targetImage = PngImageFile(img_path)
+            metadata = PngInfo()
+            metadata.add_text("User Comment", user_comment)
+            targetImage.save(img_path, pnginfo=metadata)
 
-                else:
-                    print(' Without description.')
-                    user_comment = '' # insert an empty string into the User Comment Exif field to erase <data too large to display>
-                    if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
-                        exif_dict = piexif.load(img_path)
-                        exif_dict["0th"][piexif.ImageIFD.ImageDescription] = bytes(user_comment, "utf-8")
-                        exif_dict["Exif"][piexif.ExifIFD.UserComment] = piexif.helper.UserComment.dump(user_comment,encoding='unicode')
-                        exif_bytes = piexif.dump(exif_dict) 
-                        piexif.insert(exif_bytes, img_path)
+        else:
+          print(' Without description.')
+          user_comment = '' # insert an empty string into the User Comment Exif field to erase <data too large to display>
+          if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
+            exif_dict = piexif.load(img_path)
+            exif_dict["0th"][piexif.ImageIFD.ImageDescription] = bytes(user_comment, "utf-8")
+            exif_dict["Exif"][piexif.ExifIFD.UserComment] = piexif.helper.UserComment.dump(user_comment,encoding='unicode')
+            exif_bytes = piexif.dump(exif_dict) 
+            piexif.insert(exif_bytes, img_path)
 
-                # Change file modification date
-                creation_time = media_file['mediaMetadata']['creationTime']
-                creation_time_dt = parser.parse(creation_time)
-                if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
-                    exif_dict = piexif.load(img_path)
-                    exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = creation_time_dt.strftime('%d/%m/%Y %H:%M')
-                    exif_bytes = piexif.dump(exif_dict)
-                    piexif.insert(exif_bytes, img_path)
-                access_time = time.mktime(creation_time_dt.timetuple())
-                modified_time = access_time
-                os.utime(img_path,(access_time, modified_time))
-                img_idx = img_idx + 1
+        # Change file modification date
+        creation_time = media_file['mediaMetadata']['creationTime']
+        creation_time_dt = parser.parse(creation_time)
+        if img_ext in ['.jpg','.JPG','.jpeg','.JPEG','.tiff','.TIFF','.tif','.TIF']:
+          exif_dict = piexif.load(img_path)
+          exif_dict['Exif'][piexif.ExifIFD.DateTimeOriginal] = creation_time_dt.strftime('%d/%m/%Y %H:%M')
+          exif_bytes = piexif.dump(exif_dict)
+          piexif.insert(exif_bytes, img_path)
+        access_time = time.mktime(creation_time_dt.timetuple())
+        modified_time = access_time
+        os.utime(img_path,(access_time, modified_time))
+        img_idx = img_idx + 1
 
-                f_cap.write(file_name+'\t'+user_comment+'\n')
+        f_cap.write(file_name+'\t'+user_comment+'\n')
 
-        f_cap.close()
+    f_cap.close()
 
-        # Check for duplicates
-        duplicates = [item for item, count in collections.Counter(file_names).items() if count > 1]
-        if duplicates:
-            print('Photos with duplicated file names that were not copied: ')
-            print(*duplicates, sep=', ')
+    # Check for duplicates
+    duplicates = [item for item, count in collections.Counter(file_names).items() if count > 1]
+    if duplicates:
+      print('Photos with duplicated file names that were not copied: ')
+      print(*duplicates, sep=', ')
 
-        # Check for photos that are missing on the server side
-        for _, _, lfiles in os.walk(album_dir):
-            local_files = [f for f in lfiles if not f.endswith('.csv')]
-        not_on_server = np.setdiff1d(local_files ,file_names)
-        if not_on_server.size > 0:
-            print('\nFiles that do not exist on the server:')
-            for local_file in not_on_server:
-                print(local_file)
+    # Check for photos that are missing on the server side
+    for _, _, lfiles in os.walk(album_dir):
+      local_files = [f for f in lfiles if not f.endswith('.csv')]
+    not_on_server = np.setdiff1d(local_files ,file_names)
+    if not_on_server.size > 0:
+      print('\nFiles that do not exist on the server:')
+      for local_file in not_on_server:
+        print(local_file)
 
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-a', '--albums',
-        type = str,
-        nargs = '+',
-        default = ['all'],
-        help = "A list of album names to backup. Backup everything if set to 'all'.")
-    ap.add_argument('-d', '--dir',
-        type = str,
-        default = os.path.expanduser('~/Pictures'),
-        help = "The directory where album folders are downloaded.")
-    args = ap.parse_args()
-    gphotos_backup()
+  ap = argparse.ArgumentParser()
+  ap.add_argument('-a', '--albums',
+    type = str,
+    nargs = '+',
+    default = ['all'],
+    help = "A list of album names to backup. Backup everything if set to 'all'.")
+  ap.add_argument('-d', '--dir',
+    type = str,
+    default = os.path.expanduser('~/Pictures'),
+    help = "The directory where album folders are downloaded.")
+  args = ap.parse_args()
+  gphotos_backup()
--- quick_check_albums.py
+++ quick_check_albums.py
@@ -10,113 +10,104 @@ import webbrowser
 
 
 def simple_get(url):
-    '''
-    Attempts to get the content at `url` by making an HTTP GET request.
-    If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None.
-    '''
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+  '''
+  Attempts to get the content at `url` by making an HTTP GET request.
+  If the content-type of response is some kind of HTML/XML, return the
+  text content, otherwise return None.
+  '''
+  try:
+    with closing(get(url, stream=True)) as resp:
+      if is_good_response(resp):
+        return resp.content
+      else:
         return None
 
-def is_good_response(resp):
-    '''
-    Returns True if the response seems to be HTML, False otherwise.
-    '''
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200
-            and content_type is not None 
-            and content_type.find('html') > -1)
+  except RequestException as e:
+    print('Error during requests to {0} : {1}'.format(url, str(e)))
+    return None
 
-def log_error(e):
-    '''
-    It is always a good idea to log errors. 
-    This function just prints them, but you can
-    make it do anything.
-    '''
-    print(e)
+def is_good_response(resp):
+  '''
+  Returns True if the response seems to be HTML, False otherwise.
+  '''
+  content_type = resp.headers['Content-Type'].lower()
+  return (resp.status_code == 200
+      and content_type is not None 
+      and content_type.find('html') > -1)
 
 def main():
-    web_albums = [] #web albums
-    web_size = [] #web album size
-    ext_albums = [] #extra albums
-    dl_albums = [] #local albums
-    dl_size = [] #local album size
-    ndl_albums = [] #albums that are not downloaded yet
+  web_albums = [] #web albums
+  web_size = [] #web album size
+  ext_albums = [] #extra albums
+  dl_albums = [] #local albums
+  dl_size = [] #local album size
+  ndl_albums = [] #albums that are not downloaded yet
 
-    webbrowser.open('https://photos.google.com/albums')
-    input('''The opened web site should be saved as a HTML only web page to the 
-    folder of this repo. When finished, press <Enter> to continue...''')
-    print('\nA list of all web albums:')
-    raw_html = open('Albums - Google Photos.html')
-    html = BeautifulSoup(raw_html, 'html.parser')
-    walbums = html.findAll("div", {"jsname": "apz4bc"})
-    wsize = html.findAll("div", {"class": "UV4Xae"})
-    for album, ws in zip(walbums, wsize):
-        web_albums.append(album.text)
-        web_size.append([int(s) for s in ws.text.split() if s.isdigit()][0])
-        print(album.text+' ('+ws.text+')')
+  webbrowser.open('https://photos.google.com/albums')
+  input('''The opened web site should be saved as a HTML only web page to the 
+  folder of this repo. When finished, press <Enter> to continue...''')
+  print('\nA list of all web albums:')
+  raw_html = open('Albums - Google Photos.html')
+  html = BeautifulSoup(raw_html, 'html.parser')
+  walbums = html.findAll("div", {"jsname": "apz4bc"})
+  wsize = html.findAll("div", {"class": "UV4Xae"})
+  for album, ws in zip(walbums, wsize):
+    web_albums.append(album.text)
+    web_size.append([int(s) for s in ws.text.split() if s.isdigit()][0])
+    print(album.text+' ('+ws.text+')')
 
-    print('\nA list of local albums:')
-    albums = sorted(glob.glob(args.dir+'/*/'),reverse=True)
-    for album in albums:
-        dl_album = album.split(os.sep)[-2]
-        dl_albums.append(dl_album)
-        ds = len(glob.glob(album+'*.jpg'))+len(glob.glob(album+'*.JPG'))+\
-            len(glob.glob(album+'*.jpeg'))+len(glob.glob(album+'*.JPEG'))+\
-            len(glob.glob(album+'*.png'))+len(glob.glob(album+'*.PNG'))+\
-            len(glob.glob(album+'*.avi'))+len(glob.glob(album+'*.AVI'))+\
-            len(glob.glob(album+'*.mov'))+len(glob.glob(album+'*.MOV'))+\
-            len(glob.glob(album+'*.mp4'))+len(glob.glob(album+'*.MP4'))+\
-            len(glob.glob(album+'*.wmv'))+len(glob.glob(album+'*.WMV'))
-        dl_size.append(ds)
-        print(dl_album+' ('+str(ds)+')')
+  print('\nA list of local albums:')
+  albums = sorted(glob.glob(args.dir+'/*/'),reverse=True)
+  for album in albums:
+    dl_album = album.split(os.sep)[-2]
+    dl_albums.append(dl_album)
+    ds = len(glob.glob(album+'*.jpg'))+len(glob.glob(album+'*.JPG'))+\
+      len(glob.glob(album+'*.jpeg'))+len(glob.glob(album+'*.JPEG'))+\
+      len(glob.glob(album+'*.png'))+len(glob.glob(album+'*.PNG'))+\
+      len(glob.glob(album+'*.avi'))+len(glob.glob(album+'*.AVI'))+\
+      len(glob.glob(album+'*.mov'))+len(glob.glob(album+'*.MOV'))+\
+      len(glob.glob(album+'*.mp4'))+len(glob.glob(album+'*.MP4'))+\
+      len(glob.glob(album+'*.wmv'))+len(glob.glob(album+'*.WMV'))
+    dl_size.append(ds)
+    print(dl_album+' ('+str(ds)+')')
 
-    print('\nA list of albums that were not downloaded yet:')
-    ndl_albums = np.setdiff1d(web_albums, dl_albums)
-    ndl_albums = ndl_albums.tolist()
-    for ndl_album in ndl_albums:
-        print(ndl_album)
+  print('\nA list of albums that were not downloaded yet:')
+  ndl_albums = np.setdiff1d(web_albums, dl_albums)
+  ndl_albums = ndl_albums.tolist()
+  for ndl_album in ndl_albums:
+    print(ndl_album)
 
-    print('\nA list of downladed extra albums (shared albums, deleted web albums, etc.):')
-    ext_albums = np.setdiff1d(dl_albums, web_albums)
-    ext_albums = ext_albums.tolist()
-    for ext_album in ext_albums:
-        print(ext_album)
+  print('\nA list of downladed extra albums (shared albums, deleted web albums, etc.):')
+  ext_albums = np.setdiff1d(dl_albums, web_albums)
+  ext_albums = ext_albums.tolist()
+  for ext_album in ext_albums:
+    print(ext_album)
 
-    print('\nA list of albums with different number of photos/videos:')
-    for idx, album in enumerate(dl_albums):
-        if album in web_albums:
-            ws = web_size[web_albums.index(album)] 
-            ds = dl_size[idx]
-            if ws != ds:
-                print(album+' (local: '+str(ds)+', web: '+str(ws)+')')
+  print('\nA list of albums with different number of photos/videos:')
+  for idx, album in enumerate(dl_albums):
+    if album in web_albums:
+      ws = web_size[web_albums.index(album)] 
+      ds = dl_size[idx]
+      if ws != ds:
+        print(album+' (local: '+str(ds)+', web: '+str(ws)+')')
 
-    print('\nStats:')
-    print(' - no. of web albums: '+str(len(web_albums)))
-    print(' - no. of downloaded albums: '+str(len(dl_albums)))
-    print(' - no. of albums that exist only on the server side: '+str(len(ndl_albums)))
-    print(' - no. of albums that exist only on the local side: '+str(len(ext_albums)))
+  print('\nStats:')
+  print(' - no. of web albums: '+str(len(web_albums)))
+  print(' - no. of downloaded albums: '+str(len(dl_albums)))
+  print(' - no. of albums that exist only on the server side: '+str(len(ndl_albums)))
+  print(' - no. of albums that exist only on the local side: '+str(len(ext_albums)))
 
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument(
-        '-d','--dir',
-        type = str,
-        default = os.path.expanduser('~/Pictures'),
-        help = ''' 
-        Albums directory.
-        '''
-    )
-    
-    args = ap.parse_args()
-    main()
+  ap = argparse.ArgumentParser()
+  ap.add_argument(
+    '-d','--dir',
+    type = str,
+    default = os.path.expanduser('~/Pictures'),
+    help = ''' 
+    Albums directory.
+    '''
+  )
+  args = ap.parse_args()
+  main()
 
